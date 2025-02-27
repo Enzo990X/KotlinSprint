8876904b64d78538d99e84dc8618596b65998b73
Enzo990X <bkanurkin@live.com>
2025-02-27 21:10:57 +0300

    KS-1-1 updated

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main() {
+    var numberOfOrders: Int = 75
+    val orderPlacedMessage: String = "Thank you for a purchase"
+
+    println(numberOfOrders)
+    println(orderPlacedMessage)
+
+    var numberOfEmployees: Int = 2000
+    //println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
