0f243c7e113adbe5ac86550e1677bb6c7c9ddca4
Enzo990X <bkanurkin@live.com>
2025-03-01 20:38:57 +0300

    KS-2-1 task created

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,24 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val pupil1 = Pupils(3f)
+    val pupil2 = Pupils(4f)
+    val pupil3 = Pupils(3f)
+    val pupil4 = Pupils(5f)
+
+    val averageGrade = ((pupil1.grade + pupil2.grade + pupil3.grade + pupil4.grade) / Pupils.numberOfInstance)
+
+    println("$averageGrade")
+}
+
+class Pupils(val grade: Float) {
+
+    companion object {
+        var numberOfInstance:Int = 0
+    }
+
+    init {
+        numberOfInstance++
+    }
+}
