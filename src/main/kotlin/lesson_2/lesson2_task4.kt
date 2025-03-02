91d9c0670db499b5aa7a3cbf885e396d870b5381
Enzo990X <bkanurkin@live.com>
2025-03-02 10:17:40 +0300

    KS-2-4 corrected

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -2,13 +2,15 @@ package org.example.lesson_2
 
 fun main() {
 
-    val buffGainPercentage = 0.2f
+    val buffGainPercentage = 20
     val crystalOreExtractedQuantity = 7
     val ironOreExtractedQuantity = 11
 
-    val crystalOreBonusExtractedQuantity = (crystalOreExtractedQuantity * buffGainPercentage).toInt()
-    val ironOreBonusExtractedQuantity = (ironOreExtractedQuantity * buffGainPercentage).toInt()
+    val crystalOreBonusExtractedQuantity = (crystalOreExtractedQuantity * buffGainPercentage) / TOTAL_PERCENTAGE
+    val ironOreBonusExtractedQuantity = (ironOreExtractedQuantity * buffGainPercentage) / TOTAL_PERCENTAGE
 
     println("Получено дополнительно кристаллической руды: $crystalOreBonusExtractedQuantity")
     println("Получено дополнительно железной руды: $ironOreBonusExtractedQuantity")
 }
+
+const val TOTAL_PERCENTAGE = 100
