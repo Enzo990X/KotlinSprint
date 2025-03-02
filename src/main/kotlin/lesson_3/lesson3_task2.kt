e7ef5de4569049a94d40fec8851e17b89d48fcec
Enzo990X <bkanurkin@live.com>
2025-03-02 11:47:29 +0300

    KS-3-2 task created

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var lastName = "Андреева"
+    val firstName = "Татьяна"
+    val middleName = "Сергеевна"
+    var age = 20
+
+    println("$lastName $firstName $middleName, возраст: $age")
+
+    lastName = "Сидорова"
+    age = 22
+
+    println("$lastName $firstName $middleName, возраст: $age")
+
+}
