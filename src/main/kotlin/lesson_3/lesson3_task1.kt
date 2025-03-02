3aa097357f5cf5a3b0c7527db472163d60601f3b
Enzo990X <bkanurkin@live.com>
2025-03-02 16:45:43 +0300

    KS-3-1 task corrected

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -4,10 +4,10 @@ import java.util.*
 
 fun main() {
 
-    val h = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+    val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
     val userName = System.getProperty("user.name")
 
-    val greetingMessage = when (h) {
+    val greetingMessage = when (currentHour) {
         in 6..10 -> "Доброе утро, $userName"
         in 11..17 -> "Добрый день, $userName"
         in 18..22 -> "Добрый вечер, $userName"
