523aabd7590da23290bfc43a0f14119058c2888d
Enzo990X <bkanurkin@live.com>
2025-03-02 12:57:19 +0300

    KS-3-4 task created

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,31 @@
+package org.example.lesson_3
+
+fun main() {
+/*
+    var startPosition = "E2"
+    var endPosition = "E4"
+    var moveNumber = 1
+
+    var move = "$startPosition-$endPosition; $moveNumber"
+    println(move)
+
+    startPosition = "D2"
+    endPosition = "D4"
+    moveNumber = 2
+
+    move = "$startPosition-$endPosition; $moveNumber"
+    println(move)
+*/
+    val move1 = Move("E2", "E4", 1)
+    val move2 = Move("D2", "D4", 2)
+
+    println(move1)
+    println(move2)
+}
+
+class Move(val startPosition: String, val endPosition: String, val moveNumber: Int)
+{
+    override fun toString(): String {
+        return "$startPosition-$endPosition; $moveNumber"
+    }
+}
