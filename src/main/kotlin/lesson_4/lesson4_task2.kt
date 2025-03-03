bf46ae3fa6db0768210948f56c995f95e79f5dec
Enzo990X <bkanurkin@live.com>
2025-03-03 21:10:56 +0300

    KS-4-2 task corrected

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -7,8 +7,11 @@ fun main() {
     val weight2 = 50
     val volume2 = 100
 
-    println("Груз с весом $weight1 кг и объемом $volume1 л соответствует категории 'Average': ${(weight1 > MIN_WEIGHT && weight1 < MAX_WEIGHT) && (volume1 < MAX_VOLUME)}")
-    println("Груз с весом $weight2 кг и объемом $volume2 л соответствует категории 'Average': ${(weight2 > MIN_WEIGHT && weight2 < MAX_WEIGHT) && (volume2 < MAX_VOLUME)}")
+    val checkDymensions1 = (weight1 > MIN_WEIGHT && weight1 < MAX_WEIGHT) && (volume1 < MAX_VOLUME)
+    val checkDymensions2 = (weight2 > MIN_WEIGHT && weight2 < MAX_WEIGHT) && (volume2 < MAX_VOLUME)
+
+    println("Груз с весом $weight1 кг и объемом $volume1 л соответствует категории 'Average': $checkDymensions1")
+    println("Груз с весом $weight2 кг и объемом $volume2 л соответствует категории 'Average': $checkDymensions2")
 
 }
 
