63218febebc10572faacfc6072cd00e8d6175847
Enzo990X <bkanurkin@live.com>
2025-03-04 19:57:35 +0300

    KS-4-3 task corrected 2

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -2,35 +2,23 @@ package org.example.lesson_4
 
 fun main() {
 
-    val clearSky = true
-    val tentOpen = true
-    val humidity = 20
-    val season = "winter"
+    val isSkyClear = true
+    val isTentOpen = true
+    val currentHumidity = 20
+    val currentSeason = "winter"
 
-    val compareClearSky = REQUIRED_CLEAR_SKY == clearSky
-    val compareTentOpen = REQUIRED_TENT_OPEN == tentOpen
-    val compareHumidity = REQUIRED_HUMIDITY == humidity
-    val compareSeason = REQUIRED_SEASON != season
+    val compareSky = REQUIRED_SKY_CLEAR == isSkyClear
+    val compareTent = REQUIRED_TENT_OPEN == isTentOpen
+    val compareHumidity = REQUIRED_HUMIDITY == currentHumidity
+    val compareSeason = REQUIRED_SEASON != currentSeason
 
-    val compareConditions = compareClearSky && compareTentOpen && compareHumidity && compareSeason
+    val compareConditions = compareSky && compareTent && compareHumidity && compareSeason
 
-   /*
-    val requiredCondition = Condition(true, true, 20, "winter")
-    val currentCondition = Condition(true, true, 20, "winter")
-
-    val compareClearSky = requiredCondition.clearSky == currentCondition.clearSky
-    val compareTentOpen = requiredCondition.tentOpen == currentCondition.tentOpen
-    val compareHumidity = requiredCondition.humidity == currentCondition.humidity
-    val compareSeason = requiredCondition.season != currentCondition.season
-    val compareConditions = compareClearSky && compareTentOpen && compareHumidity && compareSeason
-*/
     println("Благоприятные ли условия сейчас для роста бобовых? $compareConditions")
 
 }
 
-class Condition(val clearSky: Boolean, val tentOpen: Boolean, val humidity: Int, val season: String)
-
-const val REQUIRED_CLEAR_SKY = true
+const val REQUIRED_SKY_CLEAR = true
 const val REQUIRED_TENT_OPEN = true
 const val REQUIRED_HUMIDITY = 20
 const val REQUIRED_SEASON = "winter"
