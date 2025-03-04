d659e14bdf6dd86fd904ccfda1729cd366d47c9b
Enzo990X <bkanurkin@live.com>
2025-03-04 21:37:12 +0300

    KS-5-6 task created

---
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -0,0 +1,22 @@
+package org.example.lesson_5
+
+fun main() {
+
+    println("Введите Ваш вес в килограммах")
+    val weight = readln().toFloat()
+
+    println("Введите Ваш рост в сантиметрах")
+    val height = readln().toFloat()
+    val convertHeight = height / CM_IN_M
+
+    val index = weight / (convertHeight * convertHeight)
+
+    when {
+        index < 18.5 -> println("Недостаточная масса тела")
+        (index >= 18.5) && (index < 25) -> println("Нормальная масса тела")
+        (index >= 25) && (index < 30) -> println("Избыточная масса тела")
+        else -> println("Ожирение")
+    }
+}
+
+const val CM_IN_M = 100
