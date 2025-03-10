458509fac4019b0fd2230162829d161194fb61fa
Enzo990X <bkanurkin@live.com>
2025-03-10 20:43:41 +0300

    KS-6-2 task created

---
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_6
+
+fun main() {
+    println("Введите необходимое для отсчёта время:")
+    val getTime = readln().toInt()
+    var i = getTime
+
+    while (i <= getTime) {
+
+        println("${i--} секунд до конца")
+        Thread.sleep(1000)
+        if (i == 0) break
+    }
+
+    println("Прошло $getTime секунд")
+}
