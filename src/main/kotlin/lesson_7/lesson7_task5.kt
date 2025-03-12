3819b0243702c30e10d7261a16c39c1c9932367d
Enzo990X <bkanurkin@live.com>
2025-03-12 21:40:05 +0300

    KS-7-5 task corrected

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -1,9 +1,12 @@
 package org.example.lesson_7
 
 fun main() {
-    var symbol = (0..9).random()
-    var smallChar = ('a'..'z').random()
-    var bigChar = ('A'..'Z').random()
+    val symbolRange = FIRST_SYMBOL..LAST_SYMBOL
+    var symbol = symbolRange.random()
+    val smallCharRange = FIRST_SMALL_CHAR..LAST_SMALL_CHAR
+    var smallChar = smallCharRange.random()
+    val bigCharRange = FIRST_BIG_CHAR..LAST_BIG_CHAR
+    var bigChar = bigCharRange.random()
 
     println("Введите размер пароля. Минимум 6 символов")
     var passwordSize = readln().toInt()
@@ -13,17 +16,31 @@ fun main() {
         passwordSize = readln().toInt()
     }
 
+    val passwordChar1 = symbol
+    val passwordChar2 = smallChar
+    val passwordChar3 = bigChar
+
     val range = RANGE_START..passwordSize
     var passwordChar = listOf(symbol, smallChar, bigChar).random()
+    var password = listOf(passwordChar1, passwordChar2, passwordChar3, passwordChar)
 
     for (i in range) {
-        print("$passwordChar")
-        symbol = (0..9).random()
-        smallChar = ('a'..'z').random()
-        bigChar = ('A'..'Z').random()
+        symbol = symbolRange.random()
+        smallChar = smallCharRange.random()
+        bigChar = bigCharRange.random()
         passwordChar = listOf(symbol, smallChar, bigChar).random()
+        password = password + passwordChar
     }
+
+    val shuffledPassword = password.shuffled().joinToString("")
+    println(shuffledPassword)
 }
 
-private const val RANGE_START = 0
+private const val RANGE_START = 3
+private const val FIRST_SYMBOL = 0
+private const val LAST_SYMBOL = 9
+private const val FIRST_SMALL_CHAR = 'a'
+private const val LAST_SMALL_CHAR = 'z'
+private const val FIRST_BIG_CHAR = 'A'
+private const val LAST_BIG_CHAR = 'Z'
 
