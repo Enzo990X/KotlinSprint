745236c4c7324ee94e0b61a679cd526fe45d9a81
Enzo990X <bkanurkin@live.com>
2025-03-13 20:26:41 +0300

    KS-7-1 task corrected

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -1,22 +1,27 @@
 package org.example.lesson_7
 
 fun main() {
-    var symbol = (0..9).random()
-    var smallChar = ('a'..'z').random()
-    var bigChar = ('A'..'Z').random()
+    val symbolRange = FIRST_SYMBOL..LAST_SYMBOL
+    var symbol: Int
+    val charRange = FIRST_CHAR..LAST_CHAR
+    var char: Char
 
     val range = RANGE_START..RANGE_END
-    var passwordChar = listOf(symbol, smallChar, bigChar).random()
+    var password = ""
 
-    print("Ваш пароль: ")
-    for (i in range) {
-        print("$passwordChar")
-        symbol = (0..9).random()
-        smallChar = ('a'..'z').random()
-        bigChar = ('A'..'Z').random()
-        passwordChar = listOf(symbol, smallChar, bigChar).random()
+    for (i in range step STEP) {
+        symbol = symbolRange.random()
+        char = charRange.random()
+        password = password + symbol + char
     }
+
+    println(password)
 }
 
+private const val FIRST_SYMBOL = 0
+private const val LAST_SYMBOL = 9
+private const val FIRST_CHAR = 'a'
+private const val LAST_CHAR = 'z'
 private const val RANGE_START = 0
 private const val RANGE_END = 6
+private const val STEP = 3
