7e4c3848b7a29ad96226b531ca435169db8177e7
Enzo990X <bkanurkin@live.com>
2025-03-13 23:23:49 +0300

    KS-8-4 task corrected

--- src/main/kotlin/lesson_8/lesson8_task4.kt
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -7,16 +7,17 @@ fun main() {
 
     println("\nКакой ингредиент Вы хотите заменить?")
     var oldIngredient = readln()
+    var index = arrayOfIngredients.indexOf(oldIngredient)
 
-    while (oldIngredient !in arrayOfIngredients) {
+    while (index == -1) {
         println("Такого ингредиента нет в списке.\nКакой ингредиент Вы хотите заменить?")
         oldIngredient = readln()
+        index = arrayOfIngredients.indexOf(oldIngredient)
     }
 
     println("На какой ингредиент Вы хотите заменить его?")
     val newIngredient = readln()
 
-    val index = arrayOfIngredients.indexOf(oldIngredient)
     arrayOfIngredients[index] = newIngredient
 
     println("Готово! Вы сохранили следующий список: ${arrayOfIngredients.joinToString()}")
