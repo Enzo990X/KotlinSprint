3562b8ffdf5cab50c2447701f23424fee0c2ec67
Enzo990X <bkanurkin@live.com>
2025-03-16 00:35:31 +0300

    KS-9-5 task corrected

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -2,20 +2,19 @@ package org.example.lesson_9
 
 fun main() {
 
-    val listOfIngredients = mutableListOf<String>()
+    val setOfIngredients = mutableSetOf<String>()
 
     repeat(NUMBER_OF_INGREDIENTS) {
-        println("Введите ингредиент:")
-        var ingredient = readln()
-        while (listOfIngredients.contains(ingredient)) {
+        while (true) {
+            println("Введите ингредиент:")
+            val ingredient = readln()
+            if (setOfIngredients.add(ingredient)) break
             println("Такой ингредиент уже есть в рецепте")
-            ingredient = readln()
         }
-        listOfIngredients.add(ingredient)
     }
 
-    listOfIngredients.sort()
-    println(listOfIngredients.joinToString().replaceFirstChar { it.uppercase() } + ".")
+    val sortedIngredients = setOfIngredients.sorted()
+    println(sortedIngredients.joinToString().replaceFirstChar { it.uppercase() } + ".")
 }
 
 private const val NUMBER_OF_INGREDIENTS = 5
