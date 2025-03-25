d1f94e7d3e8bb6e58223982b510ef55d09c55a88
Enzo990X <bkanurkin@live.com>
2025-03-25 22:11:58 +0300

    KS-13-3 task created

---
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -0,0 +1,24 @@
+package org.example.lesson_13
+
+fun main() {
+    val phonebookList = mutableListOf<Phonebook>(
+        Phonebook("Денис Семенихин", 88005553535, null),
+        Phonebook("Константин Заруцкий", 88125553535, "Апрохим"),
+        Phonebook("Егор Захаров", 88005559887, "Левел"),
+        Phonebook("Иван Иванов", 88005556554, "null"),
+        Phonebook("Петр Сидоров", 88005551234, null))
+
+    val companyList = mutableSetOf<String?>()
+
+    for (phonebook in phonebookList) {
+        companyList.add(phonebook.companyName)
+    }
+
+    println("Список компаний:")
+    for (company in companyList) {
+        if (company == null) println("<не указано>")
+        else println(company)
+    }
+}
+
+private class Phonebook (val fullName: String, val phoneNumber: Long, val companyName: String?)
