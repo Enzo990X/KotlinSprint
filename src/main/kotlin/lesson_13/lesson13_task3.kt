f6e3eb189efef4994b4f9119a3ef57151f9981a8
Enzo990X <bkanurkin@live.com>
2025-04-02 22:36:58 +0300

    KS-13-3 task refactored

--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -1,12 +1,12 @@
 package org.example.lesson_13
 
 fun main() {
-    val phonebookList = mutableListOf<Phonebook>(
-        Phonebook("Денис Семенихин", 88005553535, null),
-        Phonebook("Константин Заруцкий", 88125553535, "Апрохим"),
-        Phonebook("Егор Захаров", 88005559887, "Левел"),
-        Phonebook("Иван Иванов", 88005556554, "null"),
-        Phonebook("Петр Сидоров", 88005551234, null))
+    val phonebookList = mutableListOf<Phonebook3>(
+        Phonebook3("Денис Семенихин", 88005553535, null),
+        Phonebook3("Константин Заруцкий", 88125553535, "Апрохим"),
+        Phonebook3("Егор Захаров", 88005559887, "Левел"),
+        Phonebook3("Иван Иванов", 88005556554, "null"),
+        Phonebook3("Петр Сидоров", 88005551234, null))
 
     val companyList = mutableSetOf<String?>()
 
@@ -20,4 +20,4 @@ fun main() {
     }
 }
 
-private class Phonebook (val fullName: String, val phoneNumber: Long, val companyName: String?)
+class Phonebook3 (val fullName: String, val phoneNumber: Long, val companyName: String?)
