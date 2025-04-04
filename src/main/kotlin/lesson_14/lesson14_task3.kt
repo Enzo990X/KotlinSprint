8946a03f05f1d178087401388399d5d5affb1e31
Enzo990X <bkanurkin@live.com>
2025-04-04 22:36:08 +0300

    KS-14-4 task corrected

--- src/main/kotlin/lesson_14/lesson14_task3.kt
+++ src/main/kotlin/lesson_14/lesson14_task3.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_14
 
 fun main() {
-    val rectangle1 = Figure.Rectangle("Черный", 10.0F, 5.0F)
-    val rectangle2 = Figure.Rectangle("Белый", 8.0F, 3.0F)
-    val circle1 = Figure.Circle("Черный", 3.0F)
-    val circle2 = Figure.Circle("Белый", 5.0F)
+    val rectangle1 = Rectangle("Черный", 10.0F, 5.0F)
+    val rectangle2 = Rectangle("Белый", 8.0F, 3.0F)
+    val circle1 = Circle("Черный", 3.0F)
+    val circle2 = Circle("Белый", 5.0F)
 
     val figures = listOf(rectangle1, rectangle2, circle1, circle2)
 
@@ -19,27 +19,28 @@ fun main() {
 abstract class Figure(val color: String) {
     abstract fun area(): Float
     abstract fun perimeter(): Float
+}
 
-    class Circle(color: String, val radius: Float) : Figure(color) {
-        override fun area(): Float {
-            return PI * radius * radius
-        }
-
-        override fun perimeter(): Float {
-            return MULTIPLIER * PI * radius
-        }
+class Circle(color: String, val radius: Float) : Figure(color) {
+    override fun area(): Float {
+        return PI * radius * radius
     }
 
-    class Rectangle(color: String, val length: Float, val width: Float) : Figure(color) {
-        override fun area(): Float {
-            return length * width
-        }
-
-        override fun perimeter(): Float {
-            return MULTIPLIER * (length + width)
-        }
+    override fun perimeter(): Float {
+        return MULTIPLIER * PI * radius
     }
 }
+
+class Rectangle(color: String, val length: Float, val width: Float) : Figure(color) {
+    override fun area(): Float {
+        return length * width
+    }
+
+    override fun perimeter(): Float {
+        return MULTIPLIER * (length + width)
+    }
+}
+
 
 private const val MULTIPLIER = 2
 private const val PI = 3.14F
