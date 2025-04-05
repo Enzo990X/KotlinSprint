c629fba45e04e1d494061dbc08ff436e833c31ce
Enzo990X <bkanurkin@live.com>
2025-04-05 14:15:57 +0300

    KS-14-4 task corrected

--- src/main/kotlin/lesson_14/lesson14_task4.kt
+++ src/main/kotlin/lesson_14/lesson14_task4.kt
@@ -1,39 +1,36 @@
 package org.example.lesson_14
 
 fun main() {
-    val earth = Planet("Земля", true, true, true)
-    val satellite1 = Satellite("Луна", false, false, earth)
 
-    val venera = Planet("Венера", false, true, false)
+    val satellite1 = Satellite("Луна", false, false)
+    val satellite4 = Satellite("Ганимед", false, false)
+    val satellite5 = Satellite("Каллисто", false, false)
+    val satellite6 = Satellite("Европа", false, false)
 
-    val neptun = Planet("Нептун", false, false, true)
-    val satellite2 = Satellite("Триполе", false, false, neptun)
-    val satellite3 = Satellite("Макемо", false, false, neptun)
+    val venera = Planet("Венера", false, true, listOf())
+    val earth = Planet("Земля", true, true, listOf(satellite1))
+    val jupiter = Planet("Юпитер", false, false, listOf(satellite4, satellite5, satellite6))
 
-    val jupiter = Planet("Юпитер", false, false, true)
-    val satellite4 = Satellite("Ганимед", false, false, jupiter)
-    val satellite5 = Satellite("Каллисто", false, false, jupiter)
-    val satellite6 = Satellite("Европа", false, false, jupiter)
-
-    printSatellites()
+    printSatellites(venera)
+    printSatellites(earth)
+    printSatellites(jupiter)
 }
 
-fun printSatellites() {
-    for (planet in Planet.planets) {
-        if (planet.hasSatellite) {
-            when {
-                planet.satellites.size == ONE -> {
-                    println("У планеты ${planet.name} есть спутник ${planet.satellites.joinToString { it.name }}.")
-                }
+fun printSatellites(planet: Planet) {
 
-                planet.satellites.size > ONE -> {
-                    val satellites = planet.satellites
-                    val allButLast = planet.satellites.dropLast(ONE).joinToString { it.name }
-                    println("У планеты ${planet.name} есть спутники $allButLast и ${satellites.last().name}.")
-                }
+    if (planet.satellites.isNotEmpty()) {
+        when {
+            planet.satellites.count() == ONE -> {
+                println("У планеты ${planet.name} есть спутник ${planet.satellites.joinToString { it.name }}.")
             }
-        } else println("У планеты ${planet.name} нет спутников.")
-    }
+
+            planet.satellites.count() > ONE -> {
+                val satellites = planet.satellites
+                val allButLast = planet.satellites.dropLast(ONE).joinToString { it.name }
+                println("У планеты ${planet.name} есть спутники $allButLast и ${satellites.last().name}.")
+            }
+        }
+    } else println("У планеты ${planet.name} нет спутников.")
 }
 
 open class SpaceObject(
@@ -46,31 +43,14 @@ class Planet(
     name: String,
     hasAtmosphere: Boolean,
     isLandable: Boolean,
-    val hasSatellite: Boolean,
-) : SpaceObject(name, hasAtmosphere, isLandable) {
-
-    val satellites = mutableListOf<Satellite>()
-
-    companion object {
-        val planets = mutableListOf<Planet>()
-    }
-
-    init {
-        planets.add(this)
-    }
-}
+    val satellites: List<Satellite>,
+) : SpaceObject(name, hasAtmosphere, isLandable)
 
 class Satellite(
     name: String,
     hasAtmosphere: Boolean,
     isLandable: Boolean,
-    planet: Planet,
-) : SpaceObject(name, hasAtmosphere, isLandable) {
-
-    init {
-        planet.satellites.add(this)
-    }
-}
+) : SpaceObject(name, hasAtmosphere, isLandable)
 
 private const val ONE = 1
 
