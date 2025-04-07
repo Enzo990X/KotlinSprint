a65136784601f269214d1c6c20c5d9a84891cd11
Enzo990X <bkanurkin@live.com>
2025-04-07 22:35:47 +0300

    KS-16-4 task corrected

--- src/main/kotlin/lesson_16/lesson16_task4.kt
+++ src/main/kotlin/lesson_16/lesson16_task4.kt
@@ -3,20 +3,20 @@ package org.example.lesson_16
 fun main() {
     val order = Order(1, "New")
 
-    order.changeStatus(requestChangeOrderStatus())
+    order.requestChangeOrderStatus()
     println("Статус заказа изменен на: ${order.status}.")
-}
-
-fun requestChangeOrderStatus(): String {
-    println("Получена заявка на изменение статуса заказа. Новый статус: ")
-    val newStatus = readln()
-    return newStatus
 }
 
 class Order (private val id: Int, var status: String) {
 
-    fun changeStatus(newStatus: String) {
+    private fun changeStatus(newStatus: String) {
         status = newStatus
+    }
+
+    fun requestChangeOrderStatus() {
+        println("Получена заявка на изменение статуса заказа. Новый статус: ")
+        val newStatus = readln()
+        changeStatus(newStatus)
     }
 }
 
