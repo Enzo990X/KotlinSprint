ec935fa7320d89ac2b9dc04b7fcff089bd52a8ea
Enzo990X <bkanurkin@live.com>
2025-04-09 00:23:42 +0300

    KS-18-1 task corrected

--- src/main/kotlin/lesson_18/lesson18_task1.kt
+++ src/main/kotlin/lesson_18/lesson18_task1.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_18
 
 fun main() {
-    val order1 = Order()
-    val order2 = Order()
+    val order1 = Order(1)
+    val order2 = Order(2)
 
     order1.addOrder(1, "Зелёные тапочки")
     order2.addOrder(2, listOf("Рис", "Гречка", "Морковь", "Помидоры", "Капуста"))
 }
 
-class Order {
+class Order (val id: Int) {
 
     fun addOrder(id: Int, item: String) {
         println("Заказан товар: $item.")
