c74e74394dfbde6755fa3056ed132447d67d638f
Enzo990X <bkanurkin@live.com>
2025-04-10 00:03:00 +0300

    KS-17-4 task corrected

--- src/main/kotlin/lesson_17/lesson17_task4.kt
+++ src/main/kotlin/lesson_17/lesson17_task4.kt
@@ -2,34 +2,37 @@ package org.example.lesson_17
 
 fun main() {
 
-    val package1 = Package(1, "Москва")
-    val package2 = Package(2, "Лондон")
+    val parcel1 = Parcel(1, "Москва")
+    val parcel2 = Parcel(2, "Лондон")
 
-    println(package1.numberOfLocations)
-    println(package2.numberOfLocations)
+    movePackage(parcel1)
+    movePackage(parcel2)
+
+    println(parcel1.numberOfLocations)
+    println(parcel2.numberOfLocations)
 }
 
-class Package(val id: Int, location: String) {
+fun movePackage(parcel: Parcel) {
+    println("Зарегистрировать новое перемещение посылки № ${parcel.id}?")
+    var answer = readln()
+
+    while (answer.equals("да", ignoreCase = true)) {
+        println("\nВведите новое местоположение: ")
+
+        parcel.location = readln()
+
+        println("\nЗарегистрировать новое перемещение посылки?")
+        answer = readln()
+    }
+}
+
+class Parcel(val id: Int, location: String) {
 
     var numberOfLocations: Int = 0
 
     var location: String = location
         set(value) {
-            println("Зарегистрировать новое перемещение посылки № $id?")
-            var answer = readln()
-
-            while (answer == "Да" || answer == "да") {
-                println("Введите новое местоположение: ")
-                field = readln()
-
-                numberOfLocations++
-
-                println("\nЗарегистрировать новое перемещение посылки?")
-                answer = readln()
-            }
+            field = value
+            numberOfLocations++
         }
-
-    init {
-        this.location = location
-    }
 }
