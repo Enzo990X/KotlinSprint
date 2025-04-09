6085952a78ab69d5769549ab03b466c37c6c32d8
Enzo990X <bkanurkin@live.com>
2025-04-09 20:41:54 +0300

    KS-19-2 task refactored

--- src/main/kotlin/lesson_19/lesson19_task2.kt
+++ src/main/kotlin/lesson_19/lesson19_task2.kt
@@ -8,25 +8,29 @@ fun main() {
     item1.showInfo(1)
     item2.showInfo(2)
     item3.showInfo(3)
-
-
 }
 
 enum class Categories(val categoryId: Int) {
-    CLOTHES (1) { override fun getName(): String = "Одежда" },
-    STATIONARY (2) { override fun getName(): String = "Канцтовары" },
-    OTHER (3) { override fun getName(): String = "Прочее" };
+    CLOTHES(1) {
+        override fun getName(): String = "Одежда"
+    },
+    STATIONARY(2) {
+        override fun getName(): String = "Канцтовары"
+    },
+    OTHER(3) {
+        override fun getName(): String = "Прочее"
+    };
 
-        abstract fun getName(): String
+    abstract fun getName(): String
 }
 
-    class Item(val name: String, val id: Int, var category: Int) {
+class Item(val name: String, val id: Int, var category: Int) {
 
-        fun showInfo(category: Int) {
-            when (category) {
-                1 -> println("Название: $name, категория: ${Categories.CLOTHES.getName()}")
-                2 -> println("Название: $name, категория: ${Categories.STATIONARY.getName()}")
-                3 -> println("Название: $name, категория: ${Categories.OTHER.getName()}")
-            }
+    fun showInfo(category: Int) {
+        when (category) {
+            1 -> println("Название: $name, категория: ${Categories.CLOTHES.getName()}")
+            2 -> println("Название: $name, категория: ${Categories.STATIONARY.getName()}")
+            3 -> println("Название: $name, категория: ${Categories.OTHER.getName()}")
         }
     }
+}
