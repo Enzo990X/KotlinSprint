0ddbfdee310aee2fb67736fc46e3665d2b25fdea
Enzo990X <bkanurkin@live.com>
2025-04-13 00:39:56 +0300

    KS-20-1 task corrected

--- src/main/kotlin/lesson_21/lesson21_task1.kt
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -3,17 +3,18 @@ package org.example.lesson_21
 fun main() {
 
     val string = "hello"
-    string.vowelCount()
+    val counted = string.vowelCount()
+    when (counted) {
+        ZERO -> println("В строке нет гласных букв")
+        ONE -> println("В строке $counted гласная буква")
+        in TWO..FOUR -> println("В строке $counted гласных буквы")
+        else -> println("В строке $counted гласных букв")
+    }
 }
 
-fun String.vowelCount() {
-    val vowels = this.count { it in VOWELS }
-    when (vowels) {
-        ZERO -> println("В строке нет гласных букв")
-        ONE -> println("В строке $vowels гласная буква")
-        in TWO..FOUR -> println("В строке $vowels гласных буквы")
-        else -> println("В строке $vowels гласных букв")
-    }
+fun String.vowelCount(): Int {
+    val vowelsCount = this.count { it in VOWELS }
+    return vowelsCount
 }
 
 private const val VOWELS = "aeiouyAEIOUY"
