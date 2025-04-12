cee00f24ed0eabbc920f78e00aaff2acc1da6eba
Enzo990X <bkanurkin@live.com>
2025-04-13 00:42:09 +0300

    KS-21-2 task corrected

--- src/main/kotlin/lesson_21/lesson21_task2.kt
+++ src/main/kotlin/lesson_21/lesson21_task2.kt
@@ -4,7 +4,7 @@ fun main() {
 
     val numbersList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
-    numbersList.evenNumbersSum()
+    println("Сумма четных чисел: ${numbersList.evenNumbersSum()}")
 }
 
-fun List<Int>.evenNumbersSum() = println("Сумма четных чисел: ${this.filter { it % 2 == 0 }.sumOf { it }}")
+fun List<Int>.evenNumbersSum() = this.filter { it % 2 == 0 }.sumOf { it }
